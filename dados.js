05b10155deff40df93a57ff57034cabd0fcc8260
edumacario <edumacario@gmail.com>
2024-09-09 21:10:17 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+      titulo: "Rayssa Leal",
+      descricao: "Rayssa Leal é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos de idade, ela já coleciona títulos e inspira jovens skatistas em todo o Brasil. Sua história de superação e dedicação ao esporte a tornou um ícone da nova geração.",
+      link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+      titulo: "Beatriz Souza",
+      descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e força. Ela conquistou diversas medalhas em competições nacionais e internacionais, inspirando jovens judocas a seguirem seus passos.",
+      link: "https://pt.wikipedia.org/wiki/Beatriz_Souza" 
+    },
+    {
+      titulo: "Rebeca Andrade",
+      descricao: "Rebeca Andrade é uma ginasta brasileira que encantou o mundo com sua performance nas Olimpíadas. Sua versatilidade e determinação a tornaram uma das principais atletas brasileiras da ginástica.",
+      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade" 
+    },
+    {
+      titulo: "Isaquias Queiroz",
+      descricao: "Isaquias Queiroz é um canoísta brasileiro que se tornou um dos maiores nomes da canoagem mundial. Ele conquistou diversas medalhas olímpicas e mundiais, levando o nome do Brasil ao topo do pódio.",
+      link: "https://pt.wikipedia.org/wiki/Isaquias_Queiroz" 
+    },
+  ];
+
